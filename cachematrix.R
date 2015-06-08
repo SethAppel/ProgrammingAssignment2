56fd1a8dcb9184ee8eaf176541ca7b2908c1ccee
SethAppel <seth.appel@gmail.com>
2015-06-08 13:14:38 -0400

    Prog Assignment 2
    Make and Cache Matrix Inversion

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Will make and cache a matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function (x=matrix()) {
+  i<- NULL
+  set <- function (y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function ()x
+  setinv <- function(inverse) i <<- inverse
+  getinv <- function () i
+  list (set = set, get = get, 
+        setinv = setinv, 
+        getinv = getinv)
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Will see if a new inversion is required, will use the cache if not
+
+       
+cacheSolve <- function (x, ...) {
+  i <- x$getinv()
+  if(!is.null(i)) {
+    message ("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i<-solve(data,...)
+  x$setinv(i)
+  return(i)
 }
